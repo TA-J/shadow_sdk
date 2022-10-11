f4286d633b90fa6122d0e9b5a6e07d66db9f1f93
gaohongjian <717373141@qq.com>
2022-10-11 11:31:36 +0800

    commit 推送到nexus

---
+++ app/src/main/java/com/xqhy/shadow_sdk/Test.java
@@ -0,0 +1,14 @@
+package com.xqhy.shadow_sdk;
+
+//import com.tencent.shadow.core.runtime.container.PluginContainerActivity;
+
+/**
+ * author ghj
+ * date 2022-07-13 16:17
+ * description
+ */
+public class Test {
+    public static void main(String[] args) {
+//        PluginContainerActivity
+    }
+}
